9c6409dbf36289b9400f8dfbd77e50cd297e5fd4
My-Heaven <tdse2k@gmail.com>
2022-09-29 00:07:09 +0700

    fix something

--- UtNhanDrug-BE/Controllers/AuthenticationController.cs
+++ UtNhanDrug-BE/Controllers/AuthenticationController.cs
@@ -1,8 +1,6 @@
 ﻿using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2.Responses;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
@@ -46,6 +44,7 @@ namespace UtNhanDrug_BE.Controllers
                 return Unauthorized();
             }
         }
+
         [AllowAnonymous]
         [HttpPost("auth/staffs/login")]
         [ProducesResponseType(typeof(TokenResponse), 200)]
@@ -54,6 +53,7 @@ namespace UtNhanDrug_BE.Controllers
             if (idToken == null) return BadRequest();
             try
             {
+                
                 FirebaseToken decodedToken = await FirebaseAuth.DefaultInstance
                     .VerifyIdTokenAsync(idToken);
                 string uid = decodedToken.Uid;
@@ -68,5 +68,33 @@ namespace UtNhanDrug_BE.Controllers
                 return Unauthorized();
             }
         }
+
+        [AllowAnonymous]
+        [HttpPost("auth/staffs/accounts/login")]
+        [ProducesResponseType(typeof(TokenResponse), 200)]
+        public async Task<IActionResult> LoginStaffWithUsernamePasswordAsync([FromHeader] string idToken)
+        {
+            if (idToken == null) return BadRequest();
+            try
+            {
+
+                FirebaseToken decodedToken = await FirebaseAuth.DefaultInstance
+                    .VerifyIdTokenAsync(idToken);
+                string uid = decodedToken.Uid;
+                AccessTokenModel jwtToken = _authenticationService.AuthenticateStaff(uid);
+                if (jwtToken.AccessToken.Length != 0)
+                    return Ok(jwtToken);
+                else
+                    return NotFound(new { message = "User not register" });
+            }
+            catch (Exception)
+            {
+                return Unauthorized();
+            }
+        }
+
+
+
+
     }
 }
--- UtNhanDrug-BE/Services/AuthenticationService/AuthenticationSvc.cs
+++ UtNhanDrug-BE/Services/AuthenticationService/AuthenticationSvc.cs
@@ -55,6 +55,7 @@ namespace UtNhanDrug_BE.Services.AuthenticationService
             var isBan = _userSvc.GetManager(isUserExits.Result.UserId).Result.IsBan;
             //var uidAdmin = _configuration.GetSection("AppSettings").GetSection("AdminUID").Value;
             var user = FirebaseAdmin.Auth.FirebaseAuth.DefaultInstance.GetUserAsync(uid);
+            var isAdmin = isUserExits.Result.IsAdmin;
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings").GetSection("Secret").Value);
             Console.WriteLine(_configuration.GetSection("AppSettings").GetSection("Secret").Value);
@@ -65,10 +66,10 @@ namespace UtNhanDrug_BE.Services.AuthenticationService
                     new Claim("uid", uid),
                     new Claim("userId", isUserExits.Result.UserId.ToString()),
                     new Claim("email", user.Result.Email),
-                    new Claim("name", user.Result.DisplayName),
-                    new Claim("avatar", user.Result.PhotoUrl),
+                    //new Claim("name", user.Result.DisplayName),
+                    //new Claim("avatar", user.Result.PhotoUrl),
                     new Claim("isBan", isBan.ToString()),
-                    new Claim(ClaimTypes.Role, _roleType.Manager)
+                    isAdmin ? new Claim(ClaimTypes.Role, "ADMIN") : new Claim(ClaimTypes.Role, "MANAGER")
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -125,5 +126,85 @@ namespace UtNhanDrug_BE.Services.AuthenticationService
             var jwtToken = tokenHandler.WriteToken(token);
             return jwtToken;
         }
+
+        //authen manager by username password
+        private string CreateCustomManagerByUsernamePasswordToken(string uid)
+        {
+            var userDb = LoadUser(uid);
+            var isUserExits = _userSvc.IsExitsAccount(userDb.Result.Email);
+            var isBan = _userSvc.GetManager(isUserExits.Result.UserId).Result.IsBan;
+            var user = FirebaseAdmin.Auth.FirebaseAuth.DefaultInstance.GetUserAsync(uid);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings").GetSection("Secret").Value);
+            Console.WriteLine(_configuration.GetSection("AppSettings").GetSection("Secret").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("uid", uid),
+                    new Claim("userId", isUserExits.Result.UserId.ToString()),
+                    new Claim("email", user.Result.Email),
+                    new Claim("name", user.Result.DisplayName),
+                    new Claim("avatar", user.Result.PhotoUrl),
+                    new Claim("isBan", isBan.ToString()),
+                    new Claim(ClaimTypes.Role, _roleType.Manager)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            return jwtToken;
+        }
+
+        //authen staff by username password
+        private string CreateCustomStaffByUsernamePasswordToken(string uid)
+        {
+            var userDb = LoadUser(uid);
+            var isUserExits = _userSvc.IsExitsStaff(userDb.Result.Email);
+            var isBan = _userSvc.GetStaff(isUserExits.Result.UserId).Result.IsBan;
+            //var uidAdmin = _configuration.GetSection("AppSettings").GetSection("AdminUID").Value;
+            var user = FirebaseAdmin.Auth.FirebaseAuth.DefaultInstance.GetUserAsync(uid);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings").GetSection("Secret").Value);
+            Console.WriteLine(_configuration.GetSection("AppSettings").GetSection("Secret").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("uid", uid),
+                    new Claim("userId", isUserExits.Result.UserId.ToString()),
+                    new Claim("email", user.Result.Email),
+                    new Claim("name", user.Result.DisplayName),
+                    new Claim("avatar", user.Result.PhotoUrl),
+                    new Claim("isBan", isBan.ToString()),
+                    new Claim(ClaimTypes.Role, _roleType.Staff)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            return jwtToken;
+        }
+
+        //authen manager
+        public AccessTokenModel AuthenticateManagerByUsernamePassword(string uid)
+        {
+            AccessTokenModel token = new AccessTokenModel();
+            var user = LoadUser(uid);
+            var isUserExits = _userSvc.IsExitsAccount(user.Result.Email);
+            if (isUserExits.Result != null)
+            {
+                var customTokenAsync = CreateCustomManagerToken(uid);
+                token.Message = "Successfully";
+                token.AccessToken = customTokenAsync;
+                return token;
+            }
+            token.Message = "Fail";
+            token.AccessToken = "";
+            return token;
+        }
+
     }
 }
